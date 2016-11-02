ef9165737c7ba2f16d2744b780f9f850ff2dd4de
caiocmsouza <caiocesar.msouza@gmail.com>
2016-11-02 12:29:16 +0000

    aula6

---
+++ projeto/aula6-video2/eggplant-brownie/eggplant-brownie/models/Item.swift
@@ -0,0 +1,13 @@
+import Foundation
+
+class Item {
+    
+    let name:String
+    let calories:Double
+    
+    init(name:String, calories:Double) {
+        self.name = name
+        self.calories = calories;
+    }
+    
+}
---
+++ projeto/aula6-video2/eggplant-brownie/eggplant-brownie/viewcontrollers/ViewController.swift
@@ -0,0 +1,87 @@
+//
+//  ViewController.swift
+//  eggplant-brownie
+//
+//  Created by Alura on 19/09/16.
+//  Copyright © 2016 Alura. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    @IBOutlet var nameField : UITextField?
+    @IBOutlet var happinessField : UITextField?
+    var delegate : AddAMealDelegate?
+    
+    var items = [
+        Item(name: "Eggplant", calories: 10),
+        Item(name: "Brownie", calories: 10),
+        Item(name: "Zucchini", calories: 10),
+        Item(name:"Muffin", calories:10),
+        Item(name: "Coconut oil", calories: 500),
+        Item(name: "Chocolate frosting", calories: 1000),
+        Item(name: "Chocolate chip", calories: 1000)
+        ]
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let cell = tableView.cellForRow(at: indexPath) {
+            if(cell.accessoryType == UITableViewCellAccessoryType.none) {
+                cell.accessoryType = UITableViewCellAccessoryType.checkmark
+            } else {
+                cell.accessoryType = UITableViewCellAccessoryType.none
+            }
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let row = indexPath.row
+        let item = items[row]
+        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: nil)
+        cell.textLabel!.text = item.name
+        return cell
+    }
+    
+    
+    
+    @IBAction func add() {
+        if(nameField == nil || happinessField == nil) {
+            return
+        }
+        
+        let name:String = nameField!.text!
+        
+        if let happiness = Int(happinessField!.text!) {
+            let meal = Meal(name: name, happiness: happiness)
+
+            print("eaten \(meal.name) with happiness \(meal.happiness)!")
+            
+            if(delegate == nil) {
+                return
+            }
+            
+            delegate!.add(meal)
+    
+            if let navigation = navigationController {
+                navigation.popViewController(animated: true)
+            }
+            
+        }
+    }
+    
+    
+}
+
+
+
+
+
+
+
+
+
+
